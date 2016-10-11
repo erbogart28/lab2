c22dec768688f2e384aed68f503d353a30e16a90
Evan <gthrowaway28@gmail.com>
2016-10-11 00:30:11 +0000

    Updated with bootstrap

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def goofy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160927181310) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
