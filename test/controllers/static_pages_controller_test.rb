f7b47fb365de7483f76cf99801d53aac898b134e
Evan <gthrowaway28@gmail.com>
2016-10-18 18:19:03 +0000

    ProductPageUpdate

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+ 
+   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "ToyApp"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "ToyApp"
+  end
+
+  test "should get goofy" do
+    get static_pages_goofy_url
+    assert_response :success
+    assert_select "title", "ToyApp"
   end
 
 end
