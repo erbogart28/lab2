ca0c0e6cda12a3e57d0835fe38215f28a89c6e74
Evan <gthrowaway28@gmail.com>
2016-10-24 18:55:05 +0000

    Updated depot gem

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160927181310) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
