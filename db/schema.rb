a4b52748accec2bc2b36ac48dc8a0ed3fc9a2b99
Evan <gthrowaway28@gmail.com>
2017-08-04 16:01:38 +0000

    fixed up issues. Hosted on heroku now as well

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,15 @@
 ActiveRecord::Schema.define(version: 20160927181310) do
 
   create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "string"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
